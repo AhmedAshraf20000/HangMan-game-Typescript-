f9c9e36686dc6df5d685e4b90acd59be68a1508f
ahmed <elshehry@gamil.com>
2023-08-06 03:15:11 +0300

    Finished design

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    fontFamily: {
+      sans: ['Josefin Sans', 'sans-serif'],
+      alata: ['Alata'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
